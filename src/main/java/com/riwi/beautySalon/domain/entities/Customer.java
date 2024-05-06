1b1c39a8f2ca3e924573c83c6311afb69ca40e23
mtatiana115 <mtatiana115@gmail.com>
2024-05-06 09:32:34 -0500

    feature/entities added

--- src/main/java/com/riwi/beautySalon/domain/entities/Customer.java
+++ src/main/java/com/riwi/beautySalon/domain/entities/Customer.java
@@ -2,13 +2,9 @@ package com.riwi.beautySalon.domain.entities;
 
 import java.util.List;
 
-import com.riwi.beautySalon.utils.enums.RoleEmployee;
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -21,37 +17,29 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-@Entity( name = "employee")
+@Entity(name = "customer")
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class Employee {
+public class Customer {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
-
   @Column(length = 100, nullable = false)
   private String firstName;
-
   @Column(length = 100, nullable = false)
   private String lastName;
-
-  @Column(length = 100)
-  private String email;
-
   @Column(length = 20)
   private String phone;
-
-  @Column(nullable = false)
-  @Enumerated(EnumType.STRING)
-  private RoleEmployee role;
+  @Column(length = 100)
+  private String email;
 
   @ToString.Exclude  //excluir del método to string
   @EqualsAndHashCode.Exclude  //para no ponerle identificador genérico
   @OneToMany(
-    mappedBy = "employee",
+    mappedBy = "customer",
     fetch = FetchType.EAGER,
     cascade = CascadeType.ALL,
     orphanRemoval = false
--- src/main/java/com/riwi/beautySalon/domain/entities/ServiceEntity.java
+++ src/main/java/com/riwi/beautySalon/domain/entities/ServiceEntity.java
@@ -1,17 +1,23 @@
 package com.riwi.beautySalon.domain.entities;
 
 import java.math.BigDecimal;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Lob;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity(name = "service")
 @Data
@@ -30,5 +36,15 @@ public class ServiceEntity {
   @Column(nullable = false)
   //tipo de dato entero grande y puede tener decimales
   private BigDecimal price;
+
+  @ToString.Exclude  //excluir del método to string
+  @EqualsAndHashCode.Exclude  //para no ponerle identificador genérico
+  @OneToMany(
+    mappedBy = "customer",
+    fetch = FetchType.EAGER,
+    cascade = CascadeType.ALL,
+    orphanRemoval = false
+  )
+  private List<Appointment> appointments;
 }
 
