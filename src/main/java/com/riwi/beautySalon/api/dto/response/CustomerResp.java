a60b5ba2289baf0d31b3cb6890ef0791ffd80c66
mtatiana115 <mtatiana115@gmail.com>
2024-05-07 10:10:16 -0500

    feature/dto to response added

---
+++ src/main/java/com/riwi/beautySalon/api/dto/response/CustomerResp.java
@@ -0,0 +1,21 @@
+package com.riwi.beautySalon.api.dto.response;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CustomerResp {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private String email;
+    private List<AppointmentToCustomer> appointments;
+}
